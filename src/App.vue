373aa7060bb37da76c620ec405209ae594d62730
KaverinMikhail <mih-kaverin@yandex.ru>
2024-05-16 17:27:44 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,173 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <h1>Персонажи из сериала Рик и Морти</h1>
+  </header>
+  <main>
+    <div>
+      <select name="sort" @change="sortedPage">
+        <option value="?status=">Все персонгажи</option>
+        <option value="?status=alive">Alive</option>
+        <option value="?status=unknown">Unknown</option>
+        <option value="?status=dead">Dead</option>
+      </select>
+      <input
+        type="text"
+        placeholder="Имя персонажа"
+        v-model="searchCharacterName"
+      />
+      <button @click="applySort">Поиск песронажа</button>
+    </div>
+
+    <div class="main__character__card__all">
+      <div
+        v-for="item in characterCard"
+        :key="item.id"
+        class="main__character__card__all__wrapper"
+      >
+        <div class="main__character__img">
+          <img :src="item.image" alt="#" />
+        </div>
+        <div class="main__character">
+          <strong
+            ><div class="main__character__name">{{ item.name }}</div></strong
+          >
+          <div class="main__character__status">{{ item.status }}</div>
+          <div class="main__character__species">{{ item.species }}</div>
+          <div class="main__character__location">
+            Последнее известное местоположение: {{ item.location.name }}
+          </div>
+          <div class="main__character__episode">
+            Впервые был замечен в: {{ item.episode[0] }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="nextBtn">
+      <div v-for="item in pagesCharacterAll" :key="item">
+        <button @click="nextPage(item)">{{ item }}</button>
+      </div>
+    </div>
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+const characterCard = ref([]);
+const page = ref(1);
+let pagesCharacterAll = 0;
+let paramApiName = "";
+let paramApiStatus = "?status=";
+const searchCharacterName = ref("");
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+// NEXT PAGE
+
+const nextPage = function (item) {
+  page.value = item;
+  requestApi();
+};
+
+// SORT
+
+const sortedPage = function (value) {
+  paramApiStatus = value.target.value;
+};
+
+const applySort = function () {
+  paramApiName = `&name=${searchCharacterName.value}`;
+  page.value = 1;
+  requestApi();
+};
+
+// API
+
+const requestApi = async function () {
+  const response = await axios.get(
+    `https://rickandmortyapi.com/api/character${paramApiStatus + paramApiName}`,
+    {
+      params: {
+        page: page.value,
+      },
+    }
+  );
+  characterCard.value = response.data.results;
+  pagesCharacterAll = response.data.info.pages;
+  characterCard.value.forEach((itemUrl) => {
+    episodeApi.forEach((item) => {
+      item.forEach((item) => {
+        if (itemUrl.episode[0] === item.url) {
+          itemUrl.episode[0] = item.name;
+        }
+      });
+    });
+  });
+};
+
+// ALL EPISODES
+
+let episodeApi = [];
+
+const allEpisodes = async function () {
+  let episodePage = 1;
+  do {
+    const response = await axios.get(
+      `https://rickandmortyapi.com/api/episode?page=${episodePage}`
+    );
+    episodeApi.push(response.data.results);
+    episodePage++;
+  } while (episodePage < 4);
+};
+
+allEpisodes();
+
+requestApi();
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.main__character__card__all {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  background-color: #272b33;
+}
+.main__character__card__all__wrapper {
+  border-radius: 10px;
+  background-color: #3c3e44;
+  color: white;
+  width: 30%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: flex;
+}
+.main__character__img img {
+  width: 150px;
+  height: 100%;
+  border-radius: 10px 0 0 10px;
+  margin-right: 10px;
+}
+.main__character {
+}
+.main__character__name {
+  font-size: 32px;
+  font-weight: 700;
+}
+.main__character__status {
+}
+.main__character__species {
+}
+.main__character__location {
+}
+.main__character__episode {
+}
+
+.nextBtn {
+  display: flex;
+  justify-content: center;
+}
+.nextBtn button {
+  margin: 5px;
 }
 </style>
